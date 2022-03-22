4c7efa1473b688ac263b202fa733eb122e010d65
Matt Otto <code1221@klox.net>
2022-03-21 23:12:37 -0500

    Fixed Spirit Shroud.

--- spell_list.js
+++ spell_list.js
@@ -8974,7 +8974,7 @@ var spell_list = {
      "name":"Spirit Shroud",
      "ritual":"no",
      "concentration":"yes",
-     "description":"You call forth spirits of dread, which flit around you for the spell's duration. The spirits are intangible and invulnerable.<br/>Until the spell ends, any attack you make deals 1d8 extra damage when you hit a creature within 10 feet of you. This damageis radiant, necrotic, or cold (your choice when you cast the spell). Any creature that takes this damage can't regain hit points until the start of your next turn.<br/>In addition, any creature of your choice that you can see that starts its turn within 10 feet of you has its speed reduced by 10 feet until the start of your next turn.<br/><strong>At Higher Levels.</strong> When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d8 for every two slot levels above 3rd.",
+     "description":"You call forth spirits of dread, which flit around you for the spell's duration. The spirits are intangible and invulnerable.<br/>Until the spell ends, any attack you make deals 1d8 extra damage when you hit a creature within 10 feet of you. This damage is radiant, necrotic, or cold (your choice when you cast the spell). Any creature that takes this damage can't regain hit points until the start of your next turn.<br/>In addition, any creature of your choice that you can see that starts its turn within 10 feet of you has its speed reduced by 10 feet until the start of your next turn.<br/><strong>At Higher Levels.</strong> When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d8 for every two slot levels above 3rd.",
      "full_components":"V, S"
    },
    "Summon Aberration":{
